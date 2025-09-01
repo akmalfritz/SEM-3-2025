1308dcaa7af8cb103f4bc6ea1b06bf0bd86dd144
akmalfritz <akmalfritz.02@gmail.com>
2025-09-01 11:40:50 +0700

    PBO P1

---
+++ Akaml-Test1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test1;
+
+/**
+ *
+ * @author MyBook Hype AMD
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Akmal Firdaus");
+        System.out.println("NPM: 2420506020");
+        System.out.println("Alamat: Gombong, Kebumen");
+        System.out.println("No Hp: 098282827272");
+    }
+}
